81c30b16edcd16cfced0aa94556a0f153c457b39
Lina <lina-rieger@web.de>
2025-12-10 14:19:16 +0100

    Produkte 1-5 mit KI generierten Bildern

---
+++ sql/insertProducts.sql
@@ -0,0 +1,7 @@
+-- 9 Artikel (Poster) in Datenbank einfügen
+INSERT INTO products (title, description, price, picture_path, stock) VALUES
+('Sonnenaufgang', 'Warme Farben und klare Linien.', 14.90, '../images/sonnenuntergang.png', 50),
+('Urban Nights', 'Lichter der Stadt.', 19.90, '../images/urbanNights.png', 30),
+('Bergblick', 'Minimalistische Berge.', 16.50, '../images/bergblick.png', 40),
+('Frühling', 'Blüten & Farben', 12.00, '../images/frühling.png', 60),
+('OceanBlue', 'Tiefe Blautöne', 18.75, '../images/oceanBlue.png', 25),
