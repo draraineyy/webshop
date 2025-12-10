4be0d4870ee80d446cb420f539fcdfc993ba67ad
Lina <lina-rieger@web.de>
2025-12-10 14:52:33 +0100

    Artikel 6-9 mit KI generierten Bildern in DB eingefügt

--- sql/insertProducts.sql
+++ sql/insertProducts.sql
@@ -3,5 +3,9 @@ INSERT INTO products (title, description, price, picture_path, stock) VALUES
 ('Sonnenaufgang', 'Warme Farben und klare Linien.', 14.90, '../images/sonnenuntergang.png', 50),
 ('Urban Nights', 'Lichter der Stadt.', 19.90, '../images/urbanNights.png', 30),
 ('Bergblick', 'Minimalistische Berge.', 16.50, '../images/bergblick.png', 40),
-('Frühling', 'Blüten & Farben', 12.00, '../images/frühling.png', 60),
-('OceanBlue', 'Tiefe Blautöne', 18.75, '../images/oceanBlue.png', 25),
+('Frühling', 'Blüten & Farben.', 12.00, '../images/frühling.png', 60),
+('OceanBlue', 'Tiefe Blautöne.', 18.75, '../images/oceanBlue.png', 25),
+('Waldpfad', 'Grüne Ruhe.', 15.20, '../images/waldpfad.png', 35),
+('Retro Shapes', 'Geometrie im Vintage-Stil.', 13.40, '../images/retroShapes.png', 45),
+('München Skyline', 'Abendlicht über Münchner Dächer.', 21.00, '../images/münchenSkyline', 20),
+('Nordlicht', 'Leuchtende Nacht.', 22.90, '../images/nordlicht.png', 15);
